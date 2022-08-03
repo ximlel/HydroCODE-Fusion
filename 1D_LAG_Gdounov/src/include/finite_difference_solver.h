e244ea778334dbeee0be8f613ebf9728b204ca26
Your Name <352109085@qq.com>
2022-08-03 17:27:57 +0800

    adjust all folder

--- 1D_LAG_Gdounov/src/include/finite_difference_solver.h
+++
@@ -1,5 +0,0 @@
-void Godunov_solver_source
-(double * config, int m, double * RHO[], double * U[], double * P[],
- double * E[], double * X[],
- double * MASS, double * RHOL, double * UL, double * PL,
- double * RHOR, double * UR, double * PR, double * cpu_time);
