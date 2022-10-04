8a67cf3e57b4744ecade95d301f400493c3f3e18
ximlel <qq352109085@gmail.com>
2022-10-04 22:54:54 +0800

    begin profile

--- src/hydrocode_1D/hydrocode.sh
+++ src/hydrocode_1D/hydrocode.sh
@@ -2,12 +2,15 @@
 
 ulimit -c unlimited
 
-### Compile the program
 # make clean
-make
+### Compile the program statically
+make static
 
 ### Test the program
 sh shell/hydrocode_test.sh
+
+### Compile the program
+make
 
 ### Run the program
 sh shell/hydrocode_run.sh
